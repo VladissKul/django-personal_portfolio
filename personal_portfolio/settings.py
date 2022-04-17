d4962992964d733f48fc2a98a623c2f87eb24d99
Vlad <vladislav.kulakov.2002@mail.ru>
2022-04-17 11:17:53 +0000

    Added a gitignore2

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3e$7funo7k^kqz80(2rm)i#xh%0)3=goe2@u1(^7-7+)*0cre5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vladissss.pythonanywhere.com']
 
 
 # Application definition
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
